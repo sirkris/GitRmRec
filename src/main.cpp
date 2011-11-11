7976fdc750f9de46e67fc5ece8b5a1243809729a
Kris Craig (R&D Manager) <kris.craig@onemednet.com>
2011-11-11 15:40:10 -0800

    Basic test code.  Now the fun part will be making sure everything's portable.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
-	cout << "TEST";
+	FILE *f = _popen( "ls", "r" );
+
+	if ( f == 0 )
+	{
+		fprintf( stderr, "Unable to execute!\r\n" );
+		return 1;
+	}
+
+	const int BUFSIZE = 32767;
+	char buf[BUFSIZE];
+
+	while ( fgets( buf, sizeof( buf ), f ) )
+	{
+		fprintf( stdout, "%s", buf );
+	}
 	
 	return 0;
 }
