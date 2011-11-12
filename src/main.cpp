3eff8a8e4bcd67bc4b789aca5944ffb82be2bccf
Kris Craig (R&D Manager) <kris.craig@onemednet.com>
2011-11-11 19:11:28 -0800

    Complete.  Tested successfully on Windows.  If no further commits, assume tested successfully on Linux as well.  Mac OS X will be tested later.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int main()
 {
 	FILE *f;
+	FILE *ff;
 	
 #ifdef __linux__
 	f = popen( "git status", "r" );
@@ -27,15 +28,17 @@ int main()
 
 	const int BUFSIZE = 32767;
 	char buf[BUFSIZE];
+	char buf2[BUFSIZE];
 	size_t pos;
 	string s;
 	string del = "deleted:";
+	const char *gitc;
 
 	/* Reads the output, one line at a time.  --Kris */
 	while ( fgets( buf, sizeof( buf ), f ) )
 	{
 		//fprintf( stdout, "%s", buf );
-		if ( strstr( buf, "deleted:" ) != NULL )
+		if ( strstr( buf, del.c_str() ) != NULL )
 		{
 			s = string( buf );
 
@@ -44,8 +47,37 @@ int main()
 
 			/* Trim leading tabs and/or whitespace.  --Kris */
 			s = s.erase( 0, s.find_first_not_of( " \t" ) );
+			s = s.erase( s.find_last_not_of( " \t\r\n" ) + 1 );
 
-			cout << s;
+			/* Build the Git command.  --Kris */
+			s = "git rm --cached " + s;
+
+			/* Make usable by popen.  --Kris */
+			gitc = s.c_str();
+
+			/* Execute and we're done!  --Kris */
+			cout << "Output of '" + s + "':  ";
+
+			#ifdef __linux__
+				ff = popen( gitc, "r" );
+			#elif defined __APPLE__
+				ff = popen( gitc, "r" );
+			#elif defined _WIN32 || defined __WIN64
+				ff = _popen( gitc, "r" );
+			#else
+			#error "Unknown platform!"
+			#endif
+
+			if ( ff == 0 )
+			{
+				fprintf( stderr, "Unable to execute!\r\n" );
+				return 1;
+			}
+
+			while ( fgets( buf2, sizeof( buf2 ), ff ) )
+			{
+				fprintf( stdout, "%s", buf2 );
+			}
 		}
 	}
 	
