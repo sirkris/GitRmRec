2373ed2da77787eaf343a639267330f4ed6dd904
Kris Craig (R&D Manager) <kris.craig@onemednet.com>
2011-11-11 18:09:46 -0800

    Captured the deleted paths.  Tested in Windows.  Snapshot to test on Linux.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
+#include <sstream>
 
 using namespace std;
 
@@ -8,11 +10,11 @@ int main()
 	FILE *f;
 	
 #ifdef __linux__
-	f = popen( "ls", "r" );
+	f = popen( "git status", "r" );
 #elif defined __APPLE__
-	f = popen( "ls", "r" );
+	f = popen( "git status", "r" );
 #elif defined _WIN32 || defined __WIN64
-	f = _popen( "dir", "r" );
+	f = _popen( "git status", "r" );
 #else
 #error "Unknown platform!"
 #endif
@@ -25,10 +27,26 @@ int main()
 
 	const int BUFSIZE = 32767;
 	char buf[BUFSIZE];
+	size_t pos;
+	string s;
+	string del = "deleted:";
 
+	/* Reads the output, one line at a time.  --Kris */
 	while ( fgets( buf, sizeof( buf ), f ) )
 	{
-		fprintf( stdout, "%s", buf );
+		//fprintf( stdout, "%s", buf );
+		if ( strstr( buf, "deleted:" ) != NULL )
+		{
+			s = string( buf );
+
+			pos = s.find( del ) + del.length();
+			s = s.substr( pos );
+
+			/* Trim leading tabs and/or whitespace.  --Kris */
+			s = s.erase( 0, s.find_first_not_of( " \t" ) );
+
+			cout << s;
+		}
 	}
 	
 	return 0;
